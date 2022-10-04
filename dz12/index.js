025dcc650a697349c9ca448f9aa89d2ddfa1c5f4
Kukhtina <nadiia.kukhtina@gmail.com>
2022-10-04 19:24:55 +0300

    added homework 12

---
+++ dz12/index.js
@@ -0,0 +1,32 @@
+function Calculator(arg) {
+    this.result = arg;
+
+    this.sum = function (arg) {
+        return this.result = this.result + arg;
+    };
+
+    this.mult = function (arg) {
+        return this.result = this.result * arg;
+    };
+
+    this.sub = function (arg) {
+        return this.result = this.result - arg;
+    };
+
+    this.div = function (arg) {
+        return this.result = this.result / arg;
+    };
+
+    this.set = function (arg) {
+        return this.result = arg;
+    };
+}
+
+const calc = new Calculator(10);
+calc.result;
+
+calc.sum(5);
+calc.mult(10);
+calc.sub(40);
+calc.div(10);
+calc.set(100);
